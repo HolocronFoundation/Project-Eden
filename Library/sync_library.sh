d23873475cc712a2adb2db29decb895d20b291cf
Samuel Troper <samuel.troper@holocron.foundation>
2019-06-19 23:19:57 -0400

    Added generated files sync test

--- Library/sync_library.sh
+++ Library/sync_library.sh
@@ -6,4 +6,5 @@
 # Vars
 local_storage_dir="/home/Media/Books/Project Gutenberg"
 
-rsync -av -L -K --del ftp.ibiblio.org::gutenberg "$local_storage_dir"
+rsync -av -L -K --del --progress --stats ftp.ibiblio.org::gutenberg "$local_storage_dir"
+rsync -av -L -K --del --progress --stats ftp@ftp.ibiblio.org::gutenberg-epub "/media/troper/Troper_Server-B/Gutenberg Temp"
